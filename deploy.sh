3ec975da497895f3394be7842af99bc4f4c6f452
root <root@Ubuntu-Win11>
2024-05-18 02:08:36 -0500

    feat: update main.yml

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,11 @@ CONTAINER_NAME="docker-bot_container"
 # Update the repository
 git pull
 
-# Build the image
-sudo docker build -t $IMAGE_NAME .
+# Get the ID of the last created image
+IMAGE_ID=$(sudo docker images | grep $IMAGE_NAME | awk '{print $3}' | tail -n 1)
+
+# Remove the last created image
+sudo docker rmi $IMAGE_ID || true
 
 # Stop the container
 sudo docker stop $CONTAINER_NAME || true
@@ -16,11 +19,8 @@ sudo docker stop $CONTAINER_NAME || true
 # Remove the container
 sudo docker rm $CONTAINER_NAME || true
 
-# Get the ID of the last created image
-IMAGE_ID=$(sudo docker images | grep $IMAGE_NAME | awk '{print $3}' | tail -n 1)
-
-# Remove the last created image
-sudo docker rmi $IMAGE_ID || true
+# Build the image
+sudo docker build -t $IMAGE_NAME .
 
 # Run the container
-sudo docker run -d --name $CONTAINER_NAME --env-file .env $IMAGE_NAME
+sudo docker run -d --name $CONTAINER_NAME --env-file .env $IMAGE_
