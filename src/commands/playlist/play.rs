ac53dc95c8ae43e884ed50abe6c4b5f9e56ef902
root <root@Ubuntu-Win11>
2024-06-12 22:03:46 -0500

    feat: add event client disconnect in p, play

--- src/commands/playlist/play.rs
+++ src/commands/playlist/play.rs
@@ -1,12 +1,19 @@
+use std::sync::Arc;
+
 use crate::{
-    check_msg, commands::functions::track_fun::add_track_to_queue, utils::common::get_http_client,
+    check_msg,
+    commands::{
+        events::leave_channel::ClientDisconnectNotifier, functions::track_fun::add_track_to_queue,
+    },
+    utils::common::get_http_client,
 };
 use serenity::{
-    all::{ChannelId, GuildId, Message, VoiceState},
+    all::{ChannelId, Guild, Message, UserId, VoiceState},
     builder::CreateMessage,
     client::Context,
     framework::standard::{macros::command, Args, CommandResult},
 };
+use songbird::{CoreEvent, Event};
 use tracing::warn;
 
 #[command]
@@ -22,7 +29,7 @@ async fn play_song(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
         let handler = handler_lock.lock().await;
         let _ = add_track_to_queue(msg, args, handler, ctx, http_client).await;
     } else {
-        let (new_guild_id, channel_id) = {
+        let (new_guild, channel_id) = {
             let guild = match msg.guild(&ctx.cache) {
                 Some(guild) => guild,
                 None => {
@@ -35,9 +42,8 @@ async fn play_song(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
                 .get(&msg.author.id)
                 .and_then(|voice_state: &VoiceState| voice_state.channel_id);
 
-            let guild_id: GuildId = guild.id;
-
-            (guild_id, channel_id)
+            let guild: Guild = guild.clone();
+            (guild, channel_id)
         };
 
         let connect_to = if let Some(channel) = channel_id {
@@ -53,13 +59,30 @@ async fn play_song(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
             return Ok(());
         };
 
+        let new_guild_id = new_guild.id;
+
         if let Ok(handler_lock) = manager.join(new_guild_id, connect_to).await {
-            let handler = handler_lock.lock().await;
+            let mut handler = handler_lock.lock().await;
 
             check_msg(
                 msg.channel_id
                     .say(&ctx.http, "Conectado al canal de voz")
                     .await,
+            );
+
+            let bot_id: UserId = ctx.cache.current_user().id;
+            let context_arc = Arc::new(ctx.clone());
+
+            handler.add_global_event(
+                Event::Core(CoreEvent::ClientDisconnect),
+                ClientDisconnectNotifier {
+                    bot_id,
+                    guild: new_guild,
+                    http: ctx.http.clone(),
+                    serenity_context: context_arc,
+                    songbird_context: manager.clone(),
+                    debounce: Default::default(),
+                },
             );
 
             let _ = add_track_to_queue(msg, args, handler, ctx, http_client).await;
